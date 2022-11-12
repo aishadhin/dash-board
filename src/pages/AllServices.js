e47ac816b7fda69ab36846ab4ceefb81d93385f3
aishadin <aishadhin.ac@gmail.com>
2022-11-12 09:16:04 +0600

    services added

--- src/pages/AllServices.js
+++ src/pages/AllServices.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { BiLocationPlus } from "react-icons/bi";
 
 const AllServices = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold">All services</h2>
+    <div className="w-full mx-auto">
+      <div className="bg-blue py-12 container mx-auto">
+        <div className="px-6">
+          <h1 className="text-4xl font-bold text-center text-white">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className="text-xl mt-5 text-white text-center">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique,
+            fugiat repellat? Est consequatur aperiam exercitationem.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-4xl font-bold text-center">Local SEO Services</h2>
+        <div>
+          <div>
+            <div>
+              <BiLocationPlus/>
+            </div>
+            <h3>Title</h3>
+            <p>description</p>
+            <span>price</span>
+            <button>Read More</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
